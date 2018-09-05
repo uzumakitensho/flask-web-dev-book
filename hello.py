b93eccd553b0884fb846826fb5cb92b42aa3aad8
Hafid Afridian <afridianhafid@gmail.com>
2018-09-05 11:21:13 +0700

    use flash if submited name is changed

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -29,6 +29,9 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
         session['name'] = form.name.data
         form.name.data = ''
         return redirect(url_for('index'))
