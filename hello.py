7154b667cb12a903901f89c7414a02666f754cec
Hafid Afridian <afridianhafid@gmail.com>
2018-09-05 10:58:33 +0700

    initial commit

---
+++ hello.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+
+app = Flask(__name__)
+app.env = 'development'
+app.debug = True
+app.secret_key = 'F582219E1147CEC5EF7E972747674'
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('errors/404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('errors/500.html'), 500
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+if __name__ == '__main__':
+    app.run()
